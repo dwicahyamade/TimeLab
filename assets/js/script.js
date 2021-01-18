d405ea5795db6e849d4a5faa0715588a93b801f1
Dwi Wahyu <madecahya72@gmail.com>
2021-01-18 21:45:13 +0800

    fixing overlap layout mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // Menu Toggle for Mobile
 const menu = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav-links");
+const btn_hide = document.getElementById("btn-hide");
 
 menu.addEventListener("click", function () {
   nav.classList.toggle('show');
+  btn_hide.classList.toggle('show');
   menu.classList.toggle('click');
 })
