4300e4f700e6a9dd67a81f72bc2830135e19ccf9
Dwi Wahyu <madecahya72@gmail.com>
2021-01-19 17:56:17 +0800

    add button interaction and fixed navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 // Menu Toggle for Mobile
 const menu = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav-links");
-const btn_hide = document.getElementById("btn-hide");
 
 menu.addEventListener("click", function () {
   nav.classList.toggle('show');
-  btn_hide.classList.toggle('show');
   menu.classList.toggle('click');
 })
 
@@ -13,14 +11,20 @@ menu.addEventListener("click", function () {
 $('.owl-carousel').owlCarousel({
   nav: true,
   navText: ['<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>', '<i class="fas fa-chevron-right fa-2x" aria-hidden="true"></i>'],
-  stagePadding: 100,
+  stagePadding: 30,
   center: true,
   items: 2,
   loop: true,
-  margin: 400,
+  margin: 300,
   responsive: {
-    1300: {
+    1600: {
       items: 5
+    },
+    1300: {
+      items: 4
+    },
+    1000: {
+      items: 3
     },
     700: {
       items: 2
@@ -30,3 +34,27 @@ $('.owl-carousel').owlCarousel({
     }
   }
 });
+
+// Sticky Navbar
+// Add .sticky class when scrolled
+var navbar = document.getElementById("header");
+
+window.onscroll = function () {
+  if (window.pageYOffset > 10) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+// Jquery for scroling
+$('.page-scroll').on('click', function (e) {
+  var destination = $(this).attr('href');
+  var elDestination = $(destination);
+
+  $('html').animate({
+    scrollTop: elDestination.offset().top - 80
+  }, 800);
+
+  e.preventDefault();
+})
