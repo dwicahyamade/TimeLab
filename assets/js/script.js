835a082bb1171bbc83f7db3a51460fd72c7b6ec5
Dwi Wahyu <madecahya72@gmail.com>
2021-01-19 15:40:30 +0800

    adding owl carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,25 @@ menu.addEventListener("click", function () {
   btn_hide.classList.toggle('show');
   menu.classList.toggle('click');
 })
+
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+  nav: true,
+  navText: ['<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>', '<i class="fas fa-chevron-right fa-2x" aria-hidden="true"></i>'],
+  stagePadding: 100,
+  center: true,
+  items: 2,
+  loop: true,
+  margin: 400,
+  responsive: {
+    1300: {
+      items: 5
+    },
+    700: {
+      items: 2
+    },
+    300: {
+      items: 1
+    }
+  }
+});
